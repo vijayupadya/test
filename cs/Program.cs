55295fc225c44e8e523854337d9167df363d5cdd
Vijay Upadya <41652029+vijayupadya@users.noreply.github.com>
2024-10-07 20:39:13 -0700

    Create Program.cs

---
+++ cs/Program.cs
@@ -0,0 +1,13 @@
+class Program
+{
+
+    public static void InputCode()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    public static void Main(string[] args)
+    {
+        InputCode();
+    }
+}
